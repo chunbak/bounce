066ec902222edb9eac41a4530c41ea79abc52bb6
chunbak <imone1347@gmail.com>
2024-07-16 04:44:49 +0900

    Add files via upload

---
+++ Bounce/script.js
@@ -0,0 +1,14 @@
+var main = document.getElementById("main");
+var up = document.getElementById("up");
+var down = document.getElementById("down");
+
+var con1 = document.getElementById("con1");
+
+
+var mx, my;
+
+document.addEventListener('mousemove', function(e) {
+    mx = e.clientX;
+    my = e.clientY;
+    // console.log(mx, my);
+});
